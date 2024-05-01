f10baafe22db4d5e595fae341ecbf79dc56d3feb
Pavlo Lugovy <132191934+Pavlitoo@users.noreply.github.com>
2024-05-01 14:49:43 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,17 @@ public class Main {
      * Створює нову матрицю, де останній стовпець містить суми елементів рядків вихідної матриці.
      *
      * @param matrix Вхідна матриця розміру N x M.
-     * @return Нова матриця, де останній стовпець містить суми елементів рядків.
+     * @return Нова матриця розміру N x (M + 1), де останній стовпець містить суми рядків.
      */
     public static int[][] createNewMatrixWithRowSums(int[][] matrix) {
-        // Кількість рядків та стовпців у вихідній матриці
-        int rows = matrix.length;
-        int columns = matrix[0].length;
+        int rows = matrix.length; // Кількість рядків
+        int columns = matrix[0].length; // Кількість стовпців
 
         // Створюємо нову матрицю з одним додатковим стовпцем
         int[][] newMatrix = new int[rows][columns + 1];
 
-        // Заповнюємо нову матрицю значеннями вихідної матриці і додаємо суми рядків
         for (int i = 0; i < rows; i++) {
-            // Копіюємо елементи з вихідної матриці в нову
+            // Копіюємо весь рядок у нову матрицю
             System.arraycopy(matrix[i], 0, newMatrix[i], 0, columns);
             // Останній стовпець містить суму елементів рядка
             newMatrix[i][columns] = Arrays.stream(matrix[i]).sum();
