b1104378b4879da62a77fad5b5feca563d48e4ff
Pavlo Lugovy <132191934+Pavlitoo@users.noreply.github.com>
2024-05-01 14:50:08 +0300

    MainTest.java

--- src/MainTest.java
+++ src/MainTest.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 public class MainTest {
 
     public static void main(String[] args) {
+        public static void main(String[] args) {
         // Вхідна матриця для тестування
         int[][] matrix = {
                 {1, 2, 3},
@@ -11,25 +12,12 @@ public class MainTest {
                 {7, 8, 9}
         };
 
-        // Викликаємо метод для створення нової матриці з сумами рядків в останньому стовпці
+        // Створюємо нову матрицю з сумами в останньому стовпці
         int[][] newMatrix = MatrixUtils.createNewMatrixWithRowSums(matrix);
 
-        // Виводимо вихідну матрицю
-        System.out.println("Вихідна матриця:");
-        printMatrix(matrix);
-
         // Виводимо нову матрицю
-        System.out.println("\nНова матриця з сумами в останньому стовпці:");
-        printMatrix(newMatrix);
-    }
-
-    /**
-     * Вивід двовимірної матриці на консоль.
-     *
-     * @param matrix Матриця для виводу.
-     */
-    private static void printMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
+        System.out.println("Нова матриця з сумами в останньому стовпці:");
+        for (int[] row : newMatrix) {
             System.out.println(Arrays.toString(row));
         }
     }
